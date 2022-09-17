f3ffde67f75e37024e0e1eb990f8fda49120034f
Roman Prokopchuk <71777440+ram333n@users.noreply.github.com>
2022-09-18 02:31:27 +0300

    NM: added relaxation method

--- NumericalMethods/Lab_1/main.py
+++ NumericalMethods/Lab_1/main.py
@@ -1,21 +1,30 @@
-from sympy import *
+import numpy as np
 
 # https://www.wolframalpha.com/input?i=5x%5E3+-+2x%5E2*sin%28x%29+-+2%2F5+%3D+0
 
-x = symbols('x')
-test_function = 5 * (x ** 3) - 2 * (x ** 2) * sin(x) - 2 / 5
+
+LEFT = -1.0
+RIGHT = -0.5
 EPS = 1e-5
 
 
+def test_function(x):
+    return 5 * x ** 3 - 2 * x ** 2 * np.sin(x) - 2 / 5
+
+
+def derivative(function, x, h=1e-6):
+    return (function(x + h) - function(x)) / h
+
+
 def dichotomy_method(function, a, b, eps=EPS):
-    if function.subs(x, a).evalf() * function.subs(x, b).evalf() > 0:
-        print("There's even number of roots, can't apply dichotomy method")
+    if function(a) * function(b) > 0:
+        print("Can't apply dichotomy method")
         return
 
     c = a
     while abs(b - a) > eps:
         c = (a + b) / 2
-        if function.subs(x, a).evalf() * function.subs(x, c).evalf() < 0:
+        if function(a) * function(c) < 0:
             b = c
         else:
             a = c
@@ -23,30 +32,44 @@ def dichotomy_method(function, a, b, eps=EPS):
     print(f"Found root by dichotomy method: {c}")
 
 
+def relaxation_method(function, a, b, eps=EPS):
+    n = 2000
+    points = np.linspace(a, b, n)
 
-def check_sufficient_condition(function, a, b):
-    derivative = diff(test_function, x)
-    interval = Interval(a, b)
-    min_derivative = abs(minimum(derivative, x, interval))
-    max_derivative = abs(maximum(derivative, x, interval))
-    # TODO : check sufficient condition for relaxations method
-    print(f'requirements: t є (0, 2/M1) for convergence, 2/M1: {2/ max_derivative}')
+    min_derivative = np.min(np.abs(derivative(function, points)))
+    max_derivative = np.max(np.abs(derivative(function, points)))
 
+    if min_derivative == 0:
+        print("Function doesn't satisfy sufficient condition. Can't apply relaxation method")
+        return
 
+    tau = 2 / (min_derivative + max_derivative)
+    point = a
+    prev_point = b
 
-# def relaxation_method(function, a, b, eps = EPS):
+    while abs(point - prev_point) > eps:
+        sign = 1
+        prev_point = point
+
+        if derivative(function, point) > 0:
+            sign = -1
+
+        point = prev_point + sign * tau * function(point)
+
+    print(f"Found root by relaxation method: {point}")
+
 
 def newton_method(function, a, b, eps=EPS):
-    point = b
-    next_point = a
-    derivative = diff(test_function, x)
+    point = a
+    prev_point = b
 
-    while abs(next_point - point) > eps:
-        point = next_point
-        next_point = point - function.subs(x, point).evalf() / derivative.subs(x, point).evalf()
+    while abs(point - prev_point) > eps:
+        prev_point = point
+        point = prev_point - function(prev_point) / derivative(function, prev_point)
 
-    print(f"Found root by Newton method: {next_point}")
+    print(f"Found root by Newton method: {point}")
 
 
-dichotomy_method(test_function, -5, 5)
-newton_method(test_function, -5, 5)
+dichotomy_method(test_function, LEFT, RIGHT)
+relaxation_method(test_function, LEFT, RIGHT)
+newton_method(test_function, LEFT, RIGHT)
