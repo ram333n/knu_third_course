1817f60b90d6921dc7ee32a59e906600ebc73056
Roman Prokopchuk <71777440+ram333n@users.noreply.github.com>
2022-09-30 15:37:22 +0300

    OOP: finally added task 8

--- OOP/Tasks/Task_8/src/main/java/org/example/CustomReentrantLock.java
+++ OOP/Tasks/Task_8/src/main/java/org/example/CustomReentrantLock.java
@@ -1,2 +1,48 @@
-package org.example;public class CustomReentrantLock {
+package org.example;
+
+public class CustomReentrantLock {
+    private int holdCount;
+    private long currentThreadId;
+
+    public CustomReentrantLock() {
+        this.holdCount = 0;
+    }
+
+    public synchronized void lock() {
+        if(holdCount == 0) {
+            holdCount++;
+            currentThreadId = Thread.currentThread().getId();
+        } else if (holdCount > 0 && currentThreadId == Thread.currentThread().getId()) {
+            holdCount++;
+        } else  {
+            try {
+                wait();
+                holdCount++;
+                currentThreadId = Thread.currentThread().getId();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public synchronized void unlock() {
+        if(holdCount == 0) {
+            throw new IllegalMonitorStateException("There isn't any locks in CustomReentrantLock");
+        }
+
+        holdCount--;
+
+        if(holdCount == 0) {
+            notifyAll();
+        }
+    }
+
+    public synchronized boolean tryLock() {
+        if(holdCount > 0) {
+            return false;
+        }
+
+        lock();
+        return true;
+    }
 }
--- OOP/Tasks/Task_8/src/test/java/org/example/CustomReentrantLockTest.java
+++ OOP/Tasks/Task_8/src/test/java/org/example/CustomReentrantLockTest.java
@@ -1,4 +1,33 @@
-import static org.junit.jupiter.api.Assertions.*;
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
 class CustomReentrantLockTest {
-  
+    @Test
+    void testReentrantLock() {
+        TestClass testClass = new TestClass();
+
+        Thread t1 = new Thread(testClass::increment);
+        Thread t2 = new Thread(testClass::increment);
+        Thread t3 = new Thread(testClass::increment);
+        Thread t4 = new Thread(testClass::increment);
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+
+        try {
+            t1.join();
+            t2.join();
+            t3.join();
+            t4.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertEquals(4, testClass.getCounter());
+    }
 }
