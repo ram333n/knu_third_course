2b6065fc33804e4da0bd2058b2bc1b32a07c0d84
Roman Prokopchuk <71777440+ram333n@users.noreply.github.com>
2022-10-02 23:19:51 +0300

    OOP: add XSD validator

--- OOP/Labs/Lab_2/src/main/java/org/example/Main.java
+++ OOP/Labs/Lab_2/src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import org.example.util.PathBuilder;
+import org.example.validators.XSDValidator;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(new XSDValidator().validate(PathBuilder.getPath(".", "src", "main", "resources", "input.xml"),
+                                                       PathBuilder.getPath(".", "src", "main", "resources", "schema.xsd")));
     }
 }
---
+++ OOP/Labs/Lab_2/src/main/java/org/example/parsers/CustomStAXParser.java
@@ -0,0 +1,12 @@
+package org.example.parsers;
+
+import org.example.classes.Flower;
+
+import java.util.List;
+
+public class CustomStAXParser implements Parser {
+    @Override
+    public List<Flower> parse(String pathToXML) {
+        return null;
+    }
+}
