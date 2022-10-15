68b8882e387bc01d3ebec4eafc54ffecc979bd1c
Roman Prokopchuk <71777440+ram333n@users.noreply.github.com>
2022-10-15 17:55:07 +0300

    NM: add Jacobi and Seidel methods

--- NumericalMethods/Lab_2/main.py
+++ NumericalMethods/Lab_2/main.py
@@ -1,6 +1,47 @@
-import numpy as np
+import random
 
-# def generate_random_input(dimension):
+import numpy as np
+from scipy.linalg import hilbert
+
+# https://www.wolframalpha.com/input?i=systems+of+equations+calculator
+EPS = 1e-5
+LOWER_BOUND = -50
+UPPER_BOUND = 50
+
+
+def generate_random_matrix(dimension, generate_hilbert=False):
+    if generate_hilbert:
+        return np.array(hilbert(dimension))
+
+    result = np.empty([dimension, dimension])
+
+    for i in range(dimension):
+        for j in range(dimension):
+            result[i, j] = random.randint(LOWER_BOUND, UPPER_BOUND)
+        result[i, i] = np.sum(np.abs(result[i])) + 1
+
+    return result
+
+
+def generate_random_vector(dimension):
+    result = np.zeros(dimension)
+
+    for i in range(dimension):
+        result[i] = random.randint(LOWER_BOUND, UPPER_BOUND)
+
+    return result
+
+
+def log_solution(matrix, b, solution):
+    print("Matrix: ")
+    print(matrix)
+    print("Rhs vector: ")
+    print(b)
+    print("Solution: ")
+    print(solution)
+    print("Matrix * solution: ")
+    print(matrix @ solution)
+    print(f"||Matriix * solution - b|| : {get_vector_norm(matrix @ solution - b)}")
 
 
 # Gauss method
@@ -10,32 +51,32 @@ def get_permutation_matrix(dimension, k, l):
     return result
 
 
-def get_m_matrix(A, k):
-    dimension = A.shape[0]
+def get_m_matrix(matrix, k):
+    dimension = matrix.shape[0]
     result = np.identity(dimension)
-    result[k, k] = 1 / A[k, k]
+    result[k, k] = 1 / matrix[k, k]
     for i in range(k + 1, dimension):
-        result[i, k] = -A[i, k] / A[k, k]
+        result[i, k] = -matrix[i, k] / matrix[k, k]
 
     return result
 
 
-def find_leading_element(A, column, start):
-    dimension = A.shape[0]
+def find_leading_element(matrix, column, start):
+    dimension = matrix.shape[0]
     result = start
 
     for i in range(start + 1, dimension):
-        if abs(A[i, column]) > abs(A[result, column]):
-            column = i
+        if abs(matrix[i, column]) > abs(matrix[result, column]):
+            result = i
 
     return result
 
 
-def gauss(A, b):
-    dimension = A.shape[0]
-    cur_matrix = A
-    cur_b = b
-    result = np.arange(dimension)
+def gauss_method(matrix, b):
+    dimension = matrix.shape[0]
+    cur_matrix = np.copy(matrix)
+    cur_b = np.copy(b)
+    result = np.zeros(dimension)
 
     for i in range(dimension):
         leading_idx = find_leading_element(cur_matrix, i, i)
@@ -45,16 +86,75 @@ def gauss(A, b):
         cur_matrix = m @ cur_matrix
         cur_b = m @ (p @ cur_b)
 
-    print(cur_matrix)
-
     for i in range(dimension - 1, -1, -1):
         result[i] = cur_b[i]
-        for j in range(0, i + 1):
-            cur_b[j] = cur_b[j] - cur_matrix[j, i] * result[i]
+        for j in range(0, i):
+            cur_b[j] -= cur_matrix[j, i] * result[i]
 
     return result
 
 
-test_matrix = np.array([[1, 1, 0], [1, 3, 2], [0, 1, 2]])
-test_input = np.array([1, 1, 1])
-print(gauss(test_matrix, test_input))
+# Jacobi
+def get_vector_norm(x):
+    return np.max(np.abs(x))
+
+
+def jacobi_method(matrix, b, eps=EPS):
+    dimension = b.shape[0]
+    result = np.ones(dimension)
+
+    while True:
+        prev_result = np.array(result)
+
+        for i in range(dimension):
+            delta = 0
+            for j in range(dimension):
+                if i != j:
+                    delta += matrix[i, j] * prev_result[j]
+
+            result[i] = (b[i] - delta) / matrix[i, i]
+
+        if get_vector_norm(result - prev_result) <= eps:
+            break
+
+    return result
+
+
+#Seidel
+def seidel_method(matrix, b, eps=EPS):
+    dimension = b.shape[0]
+    result = np.ones(dimension)
+
+    while True:
+        prev_result = np.array(result)
+
+        for i in range(dimension):
+            delta = 0
+
+            for j in range(dimension):
+                if j < i:
+                    delta += matrix[i, j] * result[j]
+                elif j > i:
+                    delta += matrix[i, j] * prev_result[j]
+
+            result[i] = (b[i] - delta) / matrix[i, i]
+
+        if get_vector_norm(result - prev_result) <= eps:
+            break
+
+    return result
+
+
+# test_matrix = np.array([[1, 1, 0], [1, 3, 2], [0, 1, 2]])
+# test_input = np.array([1, 1, 1])
+# log_solution(test_matrix, test_input, gauss_method(test_matrix, test_input))
+
+test_matrix = generate_random_matrix(4)
+test_b = generate_random_vector(4)
+
+print("=============================Gauss=============================")
+log_solution(test_matrix, test_b, gauss_method(test_matrix, test_b))
+print("=============================Jacobi=============================")
+log_solution(test_matrix, test_b, jacobi_method(test_matrix, test_b))
+print("=============================Seidel=============================")
+log_solution(test_matrix, test_b, seidel_method(test_matrix, test_b))
