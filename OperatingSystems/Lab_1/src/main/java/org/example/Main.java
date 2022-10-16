05ca923002c90edf4622616eb5c32020c8aa2df1
Roman Prokopchuk <71777440+ram333n@users.noreply.github.com>
2022-10-16 16:37:48 +0300

    OS: init commit

---
+++ OperatingSystems/Lab_1/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import os.lab1.compfuncs.basic.IntOps;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(IntOps.trialF(0));
+    }
+}
