73ea43d356061d49e8766c58fc62e9a0e0a916f4
Roman Prokopchuk <71777440+ram333n@users.noreply.github.com>
2022-10-20 01:16:20 +0300

    OS: add README.md and fix some bugs

--- OperatingSystems/Lab_1/src/main/java/org/example/manager/Manager.java
+++ OperatingSystems/Lab_1/src/main/java/org/example/manager/Manager.java
@@ -50,15 +50,14 @@ public class Manager {
 
         openServer();
         System.out.println("[INFO]\tOpened server");
+
         AsynchronousSocketChannel channelF = runProcess(processF, Constants.PROCESS_F_PATH);
         System.out.println("[INFO]\tF connected");
         AsynchronousSocketChannel channelG = runProcess(processG, Constants.PROCESS_G_PATH);
         System.out.println("[INFO]\tG connected");
 
-        passArgument(channelF);
-        System.out.println("[INFO]\tPassed arguments to F");
-        passArgument(channelG);
-        System.out.println("[INFO]\tPassed arguments to G");
+        passArgument(channelF, channelG);
+        System.out.println("[INFO]\tPassed arguments to clients");
 
         ByteBuffer bufferF = ByteBuffer.allocate(2 * Integer.BYTES);
         ByteBuffer bufferG = ByteBuffer.allocate(2 * Integer.BYTES);
@@ -85,22 +84,35 @@ public class Manager {
 
     private AsynchronousSocketChannel runProcess(Process process, String path)
             throws IOException, ExecutionException, InterruptedException {
-        Future<AsynchronousSocketChannel> channelFuture = serverChannel.accept();
         process = Runtime.getRuntime().
                 exec(String.format("java -cp %s %s", Constants.PATH_TO_JAR, path));
+
+        Future<AsynchronousSocketChannel> channelFuture = serverChannel.accept();
 
         return channelFuture.get();
     }
 
-    private void passArgument(AsynchronousSocketChannel channel)
+    private void passArgument(AsynchronousSocketChannel channelF,
+                              AsynchronousSocketChannel channelG)
             throws ExecutionException, InterruptedException {
 
-        ByteBuffer buffer = ByteBuffer.allocate(Integer.BYTES);
-        buffer.putInt(argument);
-        buffer.flip();
-        Future<Integer> writeFuture = channel.write(buffer);
-        writeFuture.get();
-        buffer.clear();
+        ByteBuffer bufferF = ByteBuffer.allocate(Integer.BYTES);
+        ByteBuffer bufferG = ByteBuffer.allocate(Integer.BYTES);
+
+        bufferF.putInt(argument);
+        bufferG.putInt(argument);
+
+        bufferF.flip();
+        bufferG.flip();
+
+        Future<Integer> writeFutureF = channelF.write(bufferF);
+        Future<Integer> writeFutureG = channelG.write(bufferG);
+
+        writeFutureF.get();
+        writeFutureG.get();
+
+        bufferF.clear();
+        bufferG.clear();
     }
 
     private void handleFunctionComputation(Future<Integer> future,
