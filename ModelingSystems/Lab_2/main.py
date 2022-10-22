87f8e361008623ce7fb39b5e6543a5a379625405
Roman Prokopchuk <71777440+ram333n@users.noreply.github.com>
2022-10-23 02:06:13 +0300

    MS: add Greville method

--- ModelingSystems/Lab_2/main.py
+++ ModelingSystems/Lab_2/main.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('TkAgg')
 
-input_image = np.array(image.imread('x1.bmp'), float)
-output_image = np.array(image.imread('y6.bmp'), float)
+input_image = np.array(image.imread('x1.bmp'))
+output_image = np.array(image.imread('y6.bmp'))
 EPS = 1e-12
 DELTA = 1e-8
 
@@ -35,6 +35,33 @@ def moore_penrose_method(matrix, start_delta=DELTA, eps=EPS):
     return result
 
 
+def greville_method(matrix):
+    np_matrix = np.array(matrix, float)
+    current_a = np_matrix[0:1]
+    result = np.zeros_like(current_a.T)
+
+    if np.count_nonzero(current_a[0] != 0):
+        result = current_a.T / np.dot(current_a, current_a.T)
+
+    n = np_matrix.shape[0]
+
+    for i in range(1, n):
+        current_a = matrix[i:i + 1]
+        z = np.identity(result.shape[0]) - np.dot(result, np_matrix[:i])
+        r = np.dot(result, result.T)
+        cond_expr = np.dot(np.dot(current_a, z), current_a.T)
+
+        if np.count_nonzero(cond_expr) == 1:
+            row_to_add = np.dot(z, current_a.T) / cond_expr
+        else:
+            row_to_add = np.dot(r, current_a.T) / (1 + np.dot(np.dot(current_a, r), current_a.T))
+
+        result -= np.dot(row_to_add, np.dot(current_a, result))
+        result = np.concatenate((result, row_to_add), axis=1)
+
+    return result
+
+
 def check_pseudoinverse_matrix(matrix, pseudoinverse):
     assert np.dot(np.dot(matrix, pseudoinverse), matrix).all() == matrix.all()
     assert np.dot(np.dot(pseudoinverse, matrix), pseudoinverse).all() == pseudoinverse.all()
@@ -42,8 +69,20 @@ def check_pseudoinverse_matrix(matrix, pseudoinverse):
     assert np.allclose(np.dot(pseudoinverse, matrix), np.dot(pseudoinverse, matrix).T)
 
 
-check_pseudoinverse_matrix(input_image, moore_penrose_method(input_image, DELTA))
+pseudoinverse_moore_penrouse = moore_penrose_method(input_image, DELTA)
+pseudoinverse_greville = greville_method(input_image)
 
-operator_moore_penrose = np.dot(output_image, moore_penrose_method(input_image))
+check_pseudoinverse_matrix(input_image, pseudoinverse_moore_penrouse)
+check_pseudoinverse_matrix(input_image, pseudoinverse_greville)
+
+operator_moore_penrose = np.dot(output_image, pseudoinverse_moore_penrouse)
+operator_greville = np.dot(output_image, pseudoinverse_greville)
+
+plt.imshow(input_image, 'gray')
+plt.show()
+plt.imshow(output_image, 'gray')
+plt.show()
 plt.imshow(np.dot(operator_moore_penrose, input_image), 'gray')
+plt.show()
+plt.imshow(np.dot(operator_greville, input_image), 'gray')
 plt.show()
