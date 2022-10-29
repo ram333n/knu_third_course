5acbfc618d5d26106dccd56aaad8a369c2205878
Roman Prokopchuk <71777440+ram333n@users.noreply.github.com>
2022-10-30 00:45:12 +0300

    NM: update algorithm for non-strongly connected graphs

--- NumericalMethods/Lab_2/part_2.py
+++ NumericalMethods/Lab_2/part_2.py
@@ -1,12 +1,8 @@
 import random
 import numpy as np
 
-EPS = 1e-12
+EPS = 1e-6
 DAMPING_FACTOR = 0.15
-
-
-def get_vector_norm(x):
-    return np.max(np.abs(x))
 
 
 def generate_random_webgraph(vertices_count):
@@ -14,8 +10,7 @@ def generate_random_webgraph(vertices_count):
 
     for i in range(vertices_count):
         for j in range(vertices_count):
-            if i != j:
-                result[i, j] = bool(random.getrandbits(1))
+            result[i, j] = bool(random.getrandbits(1))
 
     return result
 
@@ -27,58 +22,56 @@ def power_method_for_pagerank(matrix, eps=EPS):
         prev_result = result
         result = matrix @ result
 
-        if get_vector_norm(result - prev_result) <= eps:
+        if np.linalg.norm(result - prev_result, 1) <= eps:
             break
 
     return result
 
 
-def get_a_matrix(adjacency_matrix):
-    dimension = adjacency_matrix.shape[0]
-    diag_multiplier = np.zeros((dimension, dimension))
-
-    for i in range(dimension):
-        row_sum = np.sum(adjacency_matrix[i])
-        diag_multiplier[i, i] = 0 if row_sum == 0 else 1 / row_sum
-
-    return adjacency_matrix.T @ diag_multiplier
+def modify_adj_matrix(adj_matrix, dangling_nodes):
+    for node in dangling_nodes:
+        adj_matrix[node] = np.repeat(1, adj_matrix.shape[0])
 
 
-def get_google_matrix(adjacency_matrix, damping_factor=DAMPING_FACTOR):
-    dimension = adjacency_matrix.shape[0]
-    matrix_a = get_a_matrix(adjacency_matrix)
+def get_a_matrix(adj_matrix):
+    dangling_nodes = np.where(adj_matrix.sum(axis=1) == 0)[0]
+    modify_adj_matrix(adj_matrix, dangling_nodes)
+    return adj_matrix.T / adj_matrix.sum(axis=1)
+
+
+def get_google_matrix(adj_matrix, damping_factor=DAMPING_FACTOR):
+    dimension = adj_matrix.shape[0]
+    matrix_a = get_a_matrix(adj_matrix)
     return (1 - damping_factor) * matrix_a + damping_factor * np.ones((dimension, dimension)) / dimension
 
 
-def perform_pagerank(adjacency_matrix, damping_factor=DAMPING_FACTOR, eps=EPS):
-    matrix_m = get_google_matrix(adjacency_matrix, damping_factor)
+def perform_pagerank(adj_matrix, damping_factor=DAMPING_FACTOR, eps=EPS):
+    matrix_m = get_google_matrix(adj_matrix, damping_factor)
     return power_method_for_pagerank(matrix_m, eps)
 
 
-def log_pagerank(adjacency_matrix):
+def log_pagerank(adj_matrix):
     print("==================Adjacency matrix==================")
-    print(adjacency_matrix)
+    print(adj_matrix)
     print("==================A matrix==================")
-    print(get_a_matrix(adjacency_matrix))
+    print(get_a_matrix(adj_matrix))
     print(f"==================Google matrix(damping factor = {DAMPING_FACTOR})==================")
-    print(get_google_matrix(adjacency_matrix, DAMPING_FACTOR))
+    print(get_google_matrix(adj_matrix, DAMPING_FACTOR))
     print("==================Result==================")
-    print(perform_pagerank(adjacency_matrix))
+    print(perform_pagerank(adj_matrix))
 
-
+#
 # input = np.array([
-#     [0, 1, 0, 0],
-#     [1, 0, 0, 1],
-#     [0, 1, 0, 0],
-#     [0, 0, 1, 0]
+#     [0., 1., 1., 0., 1., 0., 0.],
+#     [0., 0., 1., 0., 1., 1., 1.],
+#     [0., 0., 0., 0., 0., 0., 0.],
+#     [1., 1., 0., 0., 0., 1., 1.],
+#     [0., 0., 0., 1., 0., 1., 1.],
+#     [1., 0., 0., 0., 1., 0., 1.],
+#     [0., 1., 0., 0., 1., 1., 0.]
 # ])
+#
 
 
-input = np.array([
-    [0, 1, 1, 0],
-    [1, 0, 0, 0],
-    [0, 1, 0, 0],
-    [1, 1, 1, 0]
-])
 
-log_pagerank(input)
+log_pagerank(generate_random_webgraph(10))
