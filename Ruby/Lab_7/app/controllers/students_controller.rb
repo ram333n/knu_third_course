ca85daff3fa907ff455d5ca40869442897bf5e17
Roman Prokopchuk <71777440+ram333n@users.noreply.github.com>
2022-11-13 22:00:49 +0200

    Ruby: add some queries

--- Ruby/Lab_7/app/controllers/students_controller.rb
+++ Ruby/Lab_7/app/controllers/students_controller.rb
@@ -42,6 +42,46 @@ class StudentsController < ApplicationController
     redirect_to root_path
   end
 
+  def get_with_academic_debt
+    @students = Student.where("geometry_score < 3 OR algebra_score < 3 OR informatics_score < 3")
+  end
+
+  def get_successful_percentage
+    @total_count = Student.count
+
+    if @total_count == 0
+      @successful_percentage = 0
+    else
+      @successful_count = Student.where("geometry_score > 3 AND algebra_score > 3 AND informatics_score > 3").count
+      @successful_percentage = @successful_count * 100 / @total_count
+    end
+  end
+
+  def get_successful_subjects
+    @scores_sum = Hash.new
+    @scores_sum["Geometry"] = Student.sum(:geometry_score)
+    @scores_sum["Algebra"] = Student.sum(:algebra_score)
+    @scores_sum["Informatics"] = Student.sum(:informatics_score)
+    @scores_sum.sort_by { |k, v| v}.reverse!
+
+    max_score_sum = @scores_sum.values[0]
+    subj_array = []
+
+    @scores_sum.each do |k, v|
+      if v != max_score_sum || max_score_sum == 0
+        break
+      end
+
+      subj_array.push(k)
+    end
+
+    if !subj_array.empty?
+      @successful_subjects = subj_array.join(", ")
+    else
+      @successful_subjects = "There isn't any student in db"
+    end
+  end
+
 private
   def student_params
     params.require(:student).permit(:surname, :group, :geometry_score, :algebra_score, :informatics_score)
--- Ruby/Lab_7/config/routes.rb
+++ Ruby/Lab_7/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root "students#index"
 
+  get "/students/academic-debt", to: "students#get_with_academic_debt"
+  get "/students/successful-percentage", to: "students#get_successful_percentage"
+  get "/students/successful-subjects", to: "students#get_successful_subjects"
   resources :students
 end
