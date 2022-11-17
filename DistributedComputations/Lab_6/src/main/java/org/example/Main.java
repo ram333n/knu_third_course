36433e1706dd80fba10686b27bfce6a1ee685c4f
Roman Prokopchuk <71777440+ram333n@users.noreply.github.com>
2022-11-18 01:47:00 +0200

    DC: add Lab 6

--- DistributedComputations/Lab_6/src/main/java/org/example/Main.java
+++ DistributedComputations/Lab_6/src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        int[] sizes = {100, 1000, 2000};
+
+        for (int matrixSize : sizes) {
+            NaiveAlgorithm.calculate(args, matrixSize);
+            TapeAlgorithm.calculate(args, matrixSize);
+            FoxAlgorithm.calculate(args, matrixSize);
+            CannonAlgorithm.calculate(args, matrixSize);
+        }
     }
 }
