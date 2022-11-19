ca468b7da3c8963224ed87410863f3c2520eddb5
Roman Prokopchuk <71777440+ram333n@users.noreply.github.com>
2022-11-19 16:34:08 +0200

    DC: add some methods for client

--- DistributedComputations/Lab_8/subtask_1/src/main/java/org/example/client/Client.java
+++ DistributedComputations/Lab_8/subtask_1/src/main/java/org/example/client/Client.java
@@ -1,9 +1,14 @@
 package org.example.client;
 
+import org.example.model.Player;
+import org.example.model.Team;
+import org.example.util.IoUtils;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.List;
 
 public class Client {
     private Socket socket;
@@ -23,4 +28,58 @@ public class Client {
         out = new DataOutputStream(socket.getOutputStream());
         System.out.println(new String(in.readAllBytes()));
     }
+
+    public void disconnect() throws IOException {
+        socket.close();
+    }
+
+    public boolean insertTeam(Team team) throws IOException {
+        IoUtils.writeString(out, "insertTeam");
+        IoUtils.writeTeam(out, team, false);
+
+        return in.readBoolean();
+    }
+
+    public boolean deleteTeam(Long id) throws IOException {
+        IoUtils.writeString(out, "deleteTeam");
+        out.writeLong(id);
+
+        return in.readBoolean();
+    }
+
+    public boolean insertPlayer(Player player) throws IOException {
+        IoUtils.writeString(out, "insertPlayer");
+        IoUtils.writePlayer(out, player, false);
+
+        return in.readBoolean();
+    }
+
+    public boolean deletePlayer(Long id) throws IOException {
+        IoUtils.writeString(out, "deletePlayer");
+        out.writeLong(id);
+
+        return in.readBoolean();
+    }
+
+    public boolean updatePlayer(Player player) throws IOException {
+        IoUtils.writeString(out, "updatePlayer");
+        IoUtils.writePlayer(out, player, true);
+
+        return in.readBoolean();
+    }
+
+    public boolean moveToAnotherTeam(Long playerId, Long newTeamId) throws IOException {
+        IoUtils.writeString(out, "moveToAnotherTeam");
+        out.writeLong(playerId);
+        out.writeLong(newTeamId);
+
+        return in.readBoolean();
+    }
+
+    //TODO: impl  List of team, players
+
+    public List<Player> findPlayersByTeamName(String teamName) throws IOException {
+        return null;
+    }
+
 }
