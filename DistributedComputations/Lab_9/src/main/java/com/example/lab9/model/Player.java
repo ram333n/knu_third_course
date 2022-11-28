1d07d16a641b61ffb054754e8df5baf3b7f6631f
Roman Prokopchuk <71777440+ram333n@users.noreply.github.com>
2022-11-28 16:50:25 +0200

    DC: setup Lab 9

---
+++ DistributedComputations/Lab_9/src/main/java/com/example/lab9/model/Player.java
@@ -0,0 +1,16 @@
+package com.example.lab9.model;
+
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Player {
+    private Long id;
+    private Long teamId;
+    private String name;
+    private BigDecimal price;
+}
---
+++ DistributedComputations/Lab_9/src/main/java/com/example/lab9/servlet/HelloServlet.java
@@ -0,0 +1,32 @@
+package com.example.lab9.servlet;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+
+    private String message;
+
+    @Override
+    public void init() {
+        message = "Hello World!";
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
