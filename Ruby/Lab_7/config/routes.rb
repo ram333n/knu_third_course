acdc5382118f6324d80ca19708ffd14a0446758c
Roman Prokopchuk <71777440+ram333n@users.noreply.github.com>
2022-12-02 00:39:01 +0200

    Ruby: fix problem with delete

--- Ruby/Lab_7/config/routes.rb
+++ Ruby/Lab_7/config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get "/students/successful-percentage", to: "students#get_successful_percentage"
   get "/students/successful-subjects", to: "students#get_successful_subjects"
   get "/students/groups-by-success", to: "students#get_groups_by_success"
+  post "students", to: "students#create"
+
   resources :students
 end
